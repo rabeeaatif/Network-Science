5cc2b2bd502c46667331cd9a045c46ba0fe2dfe1
Ghani Haider <49679471+Ghani-Haider@users.noreply.github.com>
2020-03-09 15:54:45 +0500

    Add files via upload

--- graphs.py
+++ graphs.py
@@ -1,3 +1,4 @@
+import math
 class Edge:
     """ An undirected edge. """
 
@@ -26,7 +27,7 @@ class Edge:
         """
         return f'({self.v0}, {self.v1})'
 
-    def __eq__(self, other: Edge) -> bool:
+    def __eq__(self, other) -> bool:
         """Does other have the same enpoints?
 
         Allows `==` on instances.
@@ -39,7 +40,7 @@ class Edge:
         True if other has the same endpoints as this edge.
         """
         if type(other) == type(self):
-            return self.v0, self.v1 == other.v0, other.v1
+            return (self.v0, self.v1) == (other.v0, other.v1)
         return False
 
     def __hash__(self) -> int:
@@ -82,7 +83,6 @@ class Edge:
         the other endpoint if v is an endpoint, otherwise None.
         """
         return self.v0 if v == self.v1 else self.v1 if v == self.v0 else None
-
 
 class Graph:
     """ Represents an undirected, possibly weighted, graph. """
@@ -127,7 +127,7 @@ class Graph:
         Yields:
         vertices in the graph.
         """
-        pass
+        return self.graph.vertices()
 
     def edges(self) -> {Edge}:
         """Iterates over the edges in the graph.
@@ -141,7 +141,7 @@ class Graph:
         Yields:
         vertices in the graph.
         """
-        pass
+        return self.graph.edges()
 
     def vertex_count(self) -> int:
         """Returns the number of vertices in the graph.
@@ -152,7 +152,7 @@ class Graph:
         Returns:
         the number of vertices in the graph.
         """
-        pass
+        return self.graph.vertex_count()
 
     def edge_count(self) -> int:
         """Returns the number of edges in the graph.
@@ -163,7 +163,7 @@ class Graph:
         Returns:
         the number of edges in the graph.
         """
-        pass
+        return self.graph.edge_count()
 
     def has_vertex(self, v) -> bool:
         """Returns whether v is a vertex in the graph.
@@ -175,7 +175,7 @@ class Graph:
         Returns:
         True if v is a vertex in the graph, False otherwise.
         """
-        pass
+        return self.graph.has_vertex(v)
 
     def has_edge(self, v0, v1) -> bool:
         """Returns whether the grpah contains an edge between v0 and v1.
@@ -189,7 +189,8 @@ class Graph:
         """
         assert self.has_vertex(v0) and self.has_vertex(v1), \
             f'one or more of {v0} and {v1} are not valid vertices'
-        pass
+        
+        return self.graph.has_edge(v0, v1)
 
     def has_weights(self) -> bool:
         """Returns whether the graph is weighted.
@@ -200,7 +201,7 @@ class Graph:
         Returns:
         True if the graph edges are weighted, False otherwise.
         """
-        pass
+        return self.graph.weighted
 
     def neighbors(self, v):
         """Iterates over the neighbors of the vertex v in the graph.
@@ -217,7 +218,7 @@ class Graph:
         Yields:
         neighbors of v in the graph.
         """
-        pass
+        return self.graph.neighbors(v)
 
     def degree(self, v) -> {int}:
         """Returns the degree of the vertex v in the graph.
@@ -231,4 +232,241 @@ class Graph:
         Returns:
         degree of v in the graph.
         """
-        pass
+        return self.graph.degree(v)
+    
+    def weight(self, v0: int, v1: int):
+        """Returns the weight of the edge between v0 and v1; None if no weight.
+
+        Assumes the presence of the edge between v0 and v1. Check before calling.
+
+        Args:
+        - self: the instance to operate on.
+        - v0, v1: the weight of the edge between v0 and v1 is sought.
+
+        Returns:
+        The weight of the edge between v0 and v1; None if graph is unweighted.
+        """
+        return self.graph.weight(v0, v1)
+    
+
+# --------------------------------------------------------------------------------------------------------------------- #
+
+class AdjacencyList():
+    def __init__(self, edges: str):
+        """Creates graph with the given edges
+
+        edges consists of multiple lines representing an edge list
+        representation of the graph. Each line contains 2 vertices and an
+        optional weight. All values in a line are separated by spaces. The vertices have integer values
+        and the optional weight is a float. The vertices need not begin at 0.
+
+        Args:
+        self: the instance to create.
+        edges: an edge list representation of the graph
+
+        Returns:
+        nothing."""
+        self.weighted = False # bool for weighted graphs
+        
+        self.graph_dict = {}
+
+        for edge in edges.splitlines(): 
+            if len(edge.strip().split()) == 3:
+                self.weighted = True
+            break
+
+        for edge in edges.splitlines():
+            #storing edges in lst after removing spaces, \n etc
+            if edge == '':
+                continue
+            new_edge = edge.strip().split()
+            if self.weighted == True:
+                new_edge = [float(i) for i in new_edge]
+                if new_edge[0] not in self.graph_dict.keys():
+                    self.graph_dict[new_edge[0]] = [[Edge(new_edge[0], new_edge[1]), new_edge[2]]]
+                else:
+                    self.graph_dict[new_edge[0]] = self.graph_dict[new_edge[0]] + [[Edge(new_edge[0], new_edge[1]), new_edge[2]]]
+                if new_edge[1] not in self.graph_dict.keys():
+                    self.graph_dict[new_edge[1]] = [[Edge(new_edge[1], new_edge[0]), new_edge[2]]]
+                else:
+                    self.graph_dict[new_edge[1]] = self.graph_dict[new_edge[1]] + [[Edge(new_edge[0], new_edge[1]), new_edge[2]]]
+            else:
+                new_edge = [int(i) for i in new_edge]
+                if new_edge[0] not in self.graph_dict.keys():
+                    self.graph_dict[new_edge[0]] = [Edge(new_edge[0], new_edge[1])]
+                else:
+                    self.graph_dict[new_edge[0]] = self.graph_dict[new_edge[0]] + [Edge(new_edge[0], new_edge[1])]
+                if new_edge[1] not in self.graph_dict.keys():
+                    self.graph_dict[new_edge[1]] = [Edge(new_edge[1], new_edge[0])]
+                else:
+                    self.graph_dict[new_edge[1]] = self.graph_dict[new_edge[1]] + [Edge(new_edge[0], new_edge[1])]
+            
+    def vertices(self):
+        """Iterates over the vertices in the graph.
+
+        Args:
+        - self: the instance to operate on.
+
+        Returns:
+        nothing.
+
+        Yields:
+        vertices in the graph.
+        """
+        for vertex in self.graph_dict.keys():
+            yield vertex
+
+    def edges(self) -> {Edge}:
+        """Iterates over the edges in the graph.
+
+        Args:
+        - self: the instance to operate on.
+
+        Returns:
+        nothing.
+
+        Yields:
+        vertices in the graph.
+        """
+        edge_count = []
+        for vertex in self.graph_dict.keys():
+            for edge in self.graph_dict[vertex]:
+                if edge not in edge_count:
+                    edge_count.append(edge)
+                    if self.weighted != True:
+                        yield edge
+                    else:
+                        yield edge[0]
+
+    def vertex_count(self) -> int:
+        """Returns the number of vertices in the graph.
+
+        Args:
+        - self: the instance to operate on.
+
+        Returns:
+        the number of vertices in the graph.
+        """
+        return len(self.graph_dict.keys())
+
+    def edge_count(self) -> int:
+        """Returns the number of edges in the graph.
+
+        Args:
+        - self: the instance to operate on.
+
+        Returns:
+        the number of edges in the graph.
+        """
+        counted_edges = []
+        edge_sum = 0
+        for vertex in self.graph_dict.keys():
+            for edge in self.graph_dict[vertex]:
+                if edge or edge[0] not in counted_edges:
+                    counted_edges.append(edge)
+                    edge_sum += 1
+        return edge_sum
+
+    def has_vertex(self, v) -> bool:
+        """Returns whether v is a vertex in the graph.
+
+        Args:
+        - self: the instance to operate on.
+        - v: its neighbors in the graph are to be returned.
+
+        Returns:
+        True if v is a vertex in the graph, False otherwise.
+        """
+        return (v in self.graph_dict.keys())
+
+    def has_edge(self, v0, v1) -> bool:
+        """Returns whether the grpah contains an edge between v0 and v1.
+
+        Args:
+        - self: the instance to operate on.
+        - v0, v1: does an edge exist between vertices v0 and v1 in the graph?
+
+        Returns:
+        True if an edge exists between v0 and v1 in the graph, False otherwise.
+        """
+        assert self.has_vertex(v0) and self.has_vertex(v1), \
+            f'one or more of {v0} and {v1} are not valid vertices'
+
+        for vertex in self.graph_dict.keys():
+            for edge in self.graph_dict[vertex]:
+                if Edge(v0, v1) == edge or Edge(v0, v1) == edge[0]:
+                    return True
+        return False
+
+
+    def has_weights(self) -> bool:
+        """Returns whether the graph is weighted.
+
+        Args:
+        - self: the instance to operate on.
+
+        Returns:
+        True if the graph edges are weighted, False otherwise.
+        """
+        return self.weighted
+
+    def neighbors(self, v):
+        """Iterates over the neighbors of the vertex v in the graph.
+
+        Errors if v is not in the graph. Check before calling.
+
+        Args:
+        - self: the instance to operate on.
+        - v: the vertex whose neighbors in the graph are sought.
+
+        Returns:
+        nothing.
+
+        Yields:
+        neighbors of v in the graph.
+        """
+        for vertex in self.graph_dict.keys():
+            if vertex == v:
+                continue
+            else:
+                for edge in self.graph_dict[vertex]:
+                    if edge in self.graph_dict[v]:
+                        yield vertex
+            
+
+    def degree(self, v) -> {int}:
+        """Returns the degree of the vertex v in the graph.
+
+        Errors if v is not in the graph. Check before calling.
+
+        Args:
+        - self: the instance to operate on.
+        - v: its degree in the graph is to be returned.
+
+        Returns:
+        degree of v in the graph.
+        """
+        assert self.has_vertex(v), \
+            f'{v} is not a valid vertex'
+        return len(self.graph_dict[v])
+        
+    def weight(self, v0: int, v1: int):
+        """Returns the weight of the edge between v0 and v1; None if no weight.
+
+        Assumes the presence of the edge between v0 and v1. Check before calling.
+
+        Args:
+        - self: the instance to operate on.
+        - v0, v1: the weight of the edge between v0 and v1 is sought.
+
+        Returns:
+        The weight of the edge between v0 and v1; None if graph is unweighted.
+        """
+        if self.weighted:
+            for edge in self.graph_dict[v0]:
+                if Edge(v0, v1)  == edge[0]:
+                    return edge[1]
+            return None
+        else:
+            return 1
+
